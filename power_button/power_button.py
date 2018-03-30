59a2927d621d0c1112d43b102150b36d358d1999
sksingh95 <sonu15595@hotmail.com>
2018-03-30 13:38:21 +0530

    Update power_button.py

--- power_button/power_button.py
+++ power_button/power_button.py
@@ -5,6 +5,6 @@ import subprocess
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+GPIO.wait_for_edge(5, GPIO.FALLING)
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
